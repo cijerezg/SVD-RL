Fixing reward calculation

offline/offline_train.py
@@ -94,10 +94,9 @@ class HIVES(hyper_params):
         
         error = torch.square(action - rec).mean(1)
         rec_loss = -Normal(rec, 1).log_prob(action).sum(axis=-1).mean(1)
+
         with torch.no_grad():
             weights = F.sigmoid(cum_reward)
-
-        rec_loss = rec_loss * weights
 
         if i == 0:
             wandb.log({'VAE/[encoder] STD':
@@ -109,6 +108,8 @@ class HIVES(hyper_params):
             if rec_loss.mean() < 5:
                 wandb.log({'VAE/MSE Distribution':
                            wandb.Histogram(error.detach().cpu())})
+
+        rec_loss = rec_loss * weights
 
         N = Normal(0, 1)
         kl_loss = kl_divergence(pdf, N).mean(1)
@@ -203,9 +204,9 @@ class HIVES(hyper_params):
         cum_rewards, norm_cum_rewards = compute_cum_rewards(data)
         
         data['cum_rewards'] = cum_rewards
-        data['norm_cum_rewads'] = norm_cum_rewards
+        data['norm_cum_rewards'] = norm_cum_rewards
         
-        keys = ['actions', 'observations', 'cum_rewards']
+        keys = ['actions', 'observations', 'norm_cum_rewards']
         dataset = {}
         self.max_length = self.length
 
@@ -241,7 +242,7 @@ class HIVES(hyper_params):
             seqs = seqs.reshape(-1, self.max_length, val_dim).squeeze()
             dataset[key] = seqs
 
-        dataset['cum_rewards'] = dataset['cum_rewards'][:, 0] # All cum rewards are same.
+        dataset['norm_cum_rewards'] = dataset['norm_cum_rewards'][:, 0] # All cum rewards are same.
 
         self.dataset = dataset
         self.indexes = torch.arange(self.dataset['actions'].shape[0])       

rl/agent.py
@@ -298,11 +298,6 @@ class VaLS(hyper_params):
                                                 'Q val', 'Q target', 'Reward', 'Cum reward')
             bellman_terms_ref = self.log_scatter_3d(q1, q_refs, cum_reward, idxs.unsqueeze(dim=1),
                                                     'Q val', 'Q refs', 'Cum reward', 'Idxs')
-
-            idxs_batch = torch.tensor(self.experience_buffer.idx_tracker[idxs.cpu()])
-            
-            bellman_terms_updated = self.log_scatter_3d(q1, q_target.unsqueeze(dim=1), cum_reward, idxs_batch,
-                                                        'Q val', 'Q refs', 'Cum reward', 'Iters')
             
             with torch.no_grad():
                 diff_Q = q1 - q_refs
@@ -315,8 +310,7 @@ class VaLS(hyper_params):
             wandb.log({'Critic/Distance critic to target 1': dist1,
                        'Critic/Bellman terms': bellman_terms,
                        'Critic/Bellman ref terms': bellman_terms_ref,
-                       'Critic/Diff Qs refs': diff_Qs,
-                       'Critic/Bellman terms counts': bellman_terms_updated})
+                       'Critic/Diff Qs refs': diff_Qs})
 
         q_target = rew + (0.97 * q_target).reshape(-1, 1) * (1 - dones)
         q_target = torch.clamp(q_target, min=-100, max=100)

rl/sampler.py
@@ -74,12 +74,15 @@ class Sampler(hyper_params):
                 action = action.cpu().detach().numpy()
                 action = action.squeeze()
                 obs, rew, _, done, info = self.env.step(action)
+                # Relocate environment does not use done. It uses info.
                 obs_t = torch.from_numpy(obs).to(self.device).to(torch.float32)
-                
+                done = True if done or info['success'] else False
                 # Collect trajectories
                 obs_trj.append(obs)
                 rew_trj.append(rew)
                 done_trj.append(done)
+                if done:                    
+                    break
 
         if frames is not None:
             done = True if sum(done_trj) > 0 else False